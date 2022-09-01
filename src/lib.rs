f7294b88598106bc7f23e18c649e322ae7115010
L8ng <straydragonl@foxmail.com>
2022-09-01 16:33:57 +0800

    feat: add shortcut PyFn

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use cron::Schedule;
+use pyo3::prelude::*;
+use std::str::FromStr;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn check_cron_expression(expression: String) -> PyResult<String> {
+    let result = match Schedule::from_str(expression.as_str()) {
+        Ok(_) => String::new(),
+        Err(error) => format!("Check `{}` failed, due to {:?}", &expression, error),
+    };
+    Ok(result)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pycron(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(check_cron_expression, m)?)?;
+    Ok(())
+}
